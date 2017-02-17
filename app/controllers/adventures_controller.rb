36ddb037050d7c9a78c12505ca593303886b2194
Luke <luke.e.philips@gmail.com>
2017-02-17 14:28:54 -0800

    deploy to heroku

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -14,20 +14,6 @@ class AdventuresController < ApplicationController
       render :new
     end
   end
-  def edit
-    @adventure = Adventure.find(params[:id])
-  end
-  def update
-    @place = Place.find(params[:id])
-    @adventure = Adventure.find(params[:id])
-    if @adventure.update(adventure_params)
-      flash[:notice] = "You edited #{@adventure.name}"
-      redirect_to place_path(@place)
-    else
-      flash[:alert] = @adventure.errors.full_messages.each {|m| m.to_s}.join
-      render :edit
-    end
-  end
   def destroy
     adventure = Adventure.find(params[:id])
     adventure.destroy
